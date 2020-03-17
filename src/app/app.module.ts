26e7941c6117a53cc1e4fd83f6e0762aeeef2741
jonasa64 <hvidovrefifa@gmail.com>
2020-03-17 22:28:02 +0100

    furniture component for subpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FurnitureComponent } from './furniture/furniture.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FurnitureComponent
   ],
   imports: [
     BrowserModule,
