424d860b387ac91820244125fe64fef92a5c8fdc
jonasa64 <hvidovrefifa@gmail.com>
2020-03-19 00:03:49 +0100

    Create showroom.pipe.ts

---
+++ src/app/pipes/showroom.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: "Showroom"
+})
+
+export class ShowroomPipe implements PipeTransform {
+    transform(itmes: any[], value: string) {
+        if (!value) {
+            return itmes;
+        }
+
+
+        return itmes.filter((el) => el.Showroom === value);
+    }
+}
