df69fadf934d79c29e9a92dbc04bae79013509c2
jonasa64 <hvidovrefifa@gmail.com>
2020-03-18 23:31:50 +0100

    FurnitureService and component

---
+++ src/app/furniture/furniture.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-furniture',
+  templateUrl: './furniture.component.html',
+  styleUrls: ['./furniture.component.css']
+})
+export class FurnitureComponent implements OnInit {
+@Input() funiture : { title : string,  
+                      img: string
+                      Beskrivelse : string,
+                      Vurdering : number,
+                      Showroom : string,
+                      tid : string,
+                      isDelux: boolean
+
+                    };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
