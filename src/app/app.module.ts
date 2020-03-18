b668f4178f0dda1a3bab61d3554097679b6a5793
jonasa64 <hvidovrefifa@gmail.com>
2020-03-18 23:32:22 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
-import {MatInputModule} from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatSelectModule} from '@angular/material/select'
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select'
+import { MatGridListModule } from '@angular/material/grid-list';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchComponent } from './search/search.component';
+import { FurnitureComponent } from './furniture/furniture.component';
+import { FurnitureService } from './services/furniture.service'
+import { SearchPipe } from './pipes/search.pipe'
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    FurnitureComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatSelectModule,
     MatCheckboxModule,
-    MatInputModule
+    MatInputModule,
+    MatGridListModule
   ],
-  providers: [],
+  providers: [FurnitureService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
