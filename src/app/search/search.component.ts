2a2b84a0c12af040f66367a4f58b9012e712ece4
jonasa64 <hvidovrefifa@gmail.com>
2020-03-18 23:32:12 +0100

    Search component updated

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FurnitureService } from '../services/furniture.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  furnitures = [];
+  searchValue: string;
 
-  constructor() { }
+
+  constructor(private furnitureService: FurnitureService) { }
+
+
+  /*onSearch() {
+    console.log(this.searchValue)
+    this.furnitures = this.furnitureService.getFurnitures().filter((el) => el.title.includes(this.searchValue));
+  }*/
 
   ngOnInit(): void {
+    this.furnitures = this.furnitureService.getFurnitures();
   }
 
 }
