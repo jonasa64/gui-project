d47e8563fb1e1800c29d7a47ccc19661f505eb9c
jonasa64 <hvidovrefifa@gmail.com>
2020-03-19 14:13:29 +0100

    Create valuation.pipe.ts

---
+++ src/app/pipes/valuation.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: "valuation"
+})
+
+export class ValationPipe implements PipeTransform {
+
+    transform(itmes: any[], vurdering: string) {
+        if (!vurdering) {
+            return itmes;
+        }
+
+
+
+
+        return itmes.filter((el) => el.Vurderingsort == vurdering);
+    }
+}
