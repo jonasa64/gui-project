f625ee142260ab48a2baf08071132db0853dfb86
jonasa64 <hvidovrefifa@gmail.com>
2020-03-21 00:00:47 +0100

    Update checkout.component.ts

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,13 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit,
+  AfterViewInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef
+} from '@angular/core';
+
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('cardInfo') cardInfo: ElementRef
 
-  constructor() { }
+
+  card: any;
+  cardHandler = this.onChange.bind(this);
+  error: string;
+  constructor(private cd: ChangeDetectorRef) { }
+
+  ngAfterViewInit() {
+    this.card = elements.create('card');
+    this.card.mount(this.cardInfo.nativeElement);
+
+    this.card.addEventListener('change', this.cardHandler);
+  }
+
+  ngOnDestroy() {
+    this.card.removeEventListener('change', this.cardHandler);
+    this.card.destroy();
+  }
+
+  onChange({ error }) {
+    if (error) {
+      this.error = error.message;
+    } else {
+      this.error = null;
+    }
+    this.cd.detectChanges();
+  }
+
+  async onSubmit(form: NgForm) {
+    const { token, error } = await stripe.createToken(this.card);
+
+    if (error) {
+      console.log('Something is wrong:', error);
+    } else {
+      console.log('Success!', token);
+      // ...send the token to the your backend to process the charge
+    }
+  }
 
   ngOnInit(): void {
   }
