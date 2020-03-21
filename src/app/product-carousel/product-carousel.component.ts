65545282afe63cc81593ca2dbef07279c4d33b43
Henna <qureshi.henna@gmail.com>
2020-03-21 17:49:01 +0100

    front-page

---
+++ src/app/product-carousel/product-carousel.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-carousel',
+  templateUrl: './product-carousel.component.html',
+  styleUrls: ['./product-carousel.component.css']
+})
+export class ProductCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
