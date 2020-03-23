4c4bd9e043a05b624bbd9878da22f0bfe01d3111
jonasa64 <hvidovrefifa@gmail.com>
2020-03-23 19:57:03 +0100

    creaded auth guard

---
+++ src/app/services/authguard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core'
+import { CanActivate, Router } from '@angular/router';
+import { LoginService } from '../services/login.service'
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private _loginservice: LoginService, private _router: Router) { }
+
+  canActivate() {
+    if (this._loginservice.isLoggedIn) {
+      this._router.navigate(['my-page']);
+      return true;
+    }
+    this._router.navigate(['login']);
+    return false;
+  }
+
+
+
+}
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@angular/core'
 
 @Injectable()
 export class LoginService {
+  isLoggedIn = false;
 
   login(username, password) {
     if (username === 'anne' && password === 'anne123') {
-      return true;
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
     }
 
-    return false;
+    return this.isLoggedIn;
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    return this.isLoggedIn;
   }
 
 
