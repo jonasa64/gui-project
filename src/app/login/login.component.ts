97ade663e36a4a6b3719637c14ef2694daea6044
jonasa64 <hvidovrefifa@gmail.com>
2020-03-23 12:31:29 +0100

    added html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { PasswordValidator } from './passwordvalidator'
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
 
