113445677423c3653bd45aee1f0a159ba6988bbb
jonasa64 <hvidovrefifa@gmail.com>
2020-03-23 12:33:00 +0100

    added function for can not conati space

--- src/app/login/passwordvalidator.ts
+++ src/app/login/passwordvalidator.ts
@@ -1,3 +1,12 @@
+import { FormControl } from '@angular/forms'
+
 export class PasswordValidator {
+
+  static canNotContainSpace(formControl: FormControl) {
+    if (formControl.value.indexOf(" ") >= 0) {
+      return { canNotContainSpace: true }
+    }
+    return null;
+  }
 
 }
