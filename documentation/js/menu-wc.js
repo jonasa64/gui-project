4a94f9e32b3393001b5cdbcd2daa5acd143d0cd9
Henna <qureshi.henna@gmail.com>
2020-03-24 15:19:20 +0100

    Documentation

--- documentation/js/menu-wc.js
+++ documentation/js/menu-wc.js
@@ -41,6 +41,193 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 </li>
                     </ul>
                 </li>
+                    <li class="chapter modules">
+                        <a data-type="chapter-link" href="modules.html">
+                            <div class="menu-toggler linked" data-toggle="collapse" ${ isNormalMode ?
+                                'data-target="#modules-links"' : 'data-target="#xs-modules-links"' }>
+                                <span class="icon ion-ios-archive"></span>
+                                <span class="link-name">Modules</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                        </a>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="modules-links"' : 'id="xs-modules-links"' }>
+                            <li class="link">
+                                <a href="modules/AppModule.html" data-type="entity-link">AppModule</a>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                            'data-target="#components-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' : 'data-target="#xs-components-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' }>
+                                            <span class="icon ion-md-cog"></span>
+                                            <span>Components</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' :
+                                            'id="xs-components-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' }>
+                                            <li class="link">
+                                                <a href="components/AppComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">AppComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/CheckoutComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">CheckoutComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/ConfirmationComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">ConfirmationComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/CreateUserComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">CreateUserComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/FrontPageComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">FrontPageComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/FurnitureComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">FurnitureComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/LoginComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">LoginComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/MyPageComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">MyPageComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/NotFoundComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">NotFoundComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/ProductCarouselComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">ProductCarouselComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/SearchComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">SearchComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/SingleProductComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">SingleProductComponent</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                <li class="chapter inner">
+                                    <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                        'data-target="#injectables-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' : 'data-target="#xs-injectables-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' }>
+                                        <span class="icon ion-md-arrow-round-down"></span>
+                                        <span>Injectables</span>
+                                        <span class="icon ion-ios-arrow-down"></span>
+                                    </div>
+                                    <ul class="links collapse" ${ isNormalMode ? 'id="injectables-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' :
+                                        'id="xs-injectables-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' }>
+                                        <li class="link">
+                                            <a href="injectables/LoginService.html"
+                                                data-type="entity-link" data-context="sub-entity" data-context-id="modules" }>LoginService</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                            'data-target="#pipes-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' : 'data-target="#xs-pipes-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' }>
+                                            <span class="icon ion-md-add"></span>
+                                            <span>Pipes</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="pipes-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' :
+                                            'id="xs-pipes-links-module-AppModule-485649ec2edf0bcd343a9202e84c76df"' }>
+                                            <li class="link">
+                                                <a href="pipes/CategoryPipe.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">CategoryPipe</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="pipes/SearchPipe.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">SearchPipe</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="pipes/ShowroomPipe.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">ShowroomPipe</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="pipes/TimehPipe.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">TimehPipe</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="pipes/ValationPipe.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">ValationPipe</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                            </li>
+                            <li class="link">
+                                <a href="modules/AppRoutingModule.html" data-type="entity-link">AppRoutingModule</a>
+                            </li>
+                </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#components-links"' :
+                            'data-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/FooterComponent.html" data-type="entity-link">FooterComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HeaderComponent.html" data-type="entity-link">HeaderComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#classes-links"' :
+                            'data-target="#xs-classes-links"' }>
+                            <span class="icon ion-ios-paper"></span>
+                            <span>Classes</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                            <li class="link">
+                                <a href="classes/AppPage.html" data-type="entity-link">AppPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/FurnitureService.html" data-type="entity-link">FurnitureService</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/PasswordValidator.html" data-type="entity-link">PasswordValidator</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#injectables-links"' :
+                                'data-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/CardValidator.html" data-type="entity-link">CardValidator</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#miscellaneous-links"'
+                            : 'data-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
+                        </li>
                     <li class="chapter">
                         <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
                     </li>
--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -10,10 +10,6 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 export class CreateUserComponent implements OnInit {
 
   title = 'Opret en bruger';
-  // angForm: FormGroup;
-  // constructor(private fb: FormBuilder) {
-  //   this.getErrorMessage();
-  // }
 
   name = new FormControl('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
@@ -48,58 +44,6 @@ export class CreateUserComponent implements OnInit {
 
   hide = true;
 
-  // createForm() {
-  //   this.angForm = this.fb.group({
-  //     name: ['', Validators.required],
-  //     lastname: ['', Validators.required],
-  //     email: ['', Validators.required],
-  //     number: ['', Validators.required],
-  //     password: ['', Validators.required],
-  //     repeatpassword: ['', Validators.required]
-  //   });
-  // }
-
   ngOnInit() {
   }
-
 }
-
-
-
-
-// import { Component } from '@angular/core';
-// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-// import { AuthGuard } from '../services/authguard.service';
-
-
-// @Component({
-//   selector: 'app-create-user',
-//   templateUrl: './create-user.component.html',
-//   styleUrls: ['./create-user.component.css']
-// })
-// export class CreateUserComponent {
-//   title = 'Opret en bruger';
-//   angForm: FormGroup;
-//   constructor(private fb: FormBuilder) {
-//     this.createForm();
-//   }
-
-//   getErrorMessage() {
-//     if (this.angForm.hasError('required')) {
-//       return 'You must enter a value';
-//     }
-
-//     return this.angForm.hasError('email') ? 'Not a valid email' : '';
-//   }
-
-//   createForm() {
-//     this.angForm = this.fb.group({
-//       name: ['', Validators.required],
-//       lastname: ['', Validators.required],
-//       email: ['', Validators.required],
-//       number: ['', Validators.required],
-//       password: ['', Validators.required],
-//       repeatpassword: ['', Validators.required]
-//     });
-//   }
-// }
